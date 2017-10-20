8f4b7bd866d2c7ef8916fadda53bc5c71e4cb007
Greg <gregory.pratt@me.com>
2017-10-20 16:32:57 +0100

    temp home page

--- src/components/site/blaze-site.tsx
+++ src/components/site/blaze-site.tsx
@@ -9,15 +9,20 @@ export class BlazeSite {
   render() {
     return (
       <div class="c-text o-container o-container--large u-pillar-box u-pillar-box--medium">
-        <site-header/>
+        <site-header />
         <main>
           <stencil-router id="blaze-content">
-            <stencil-route url="/" component="home-page" exact={true}/>
+
+            <stencil-route url="/"
+              component="install-page"
+              componentProps={{ name: 'Install' }}
+              exact={true} />
+
             {allRoutes.map(({ routes }) => (
               routes.map(route => (
                 <stencil-route url={route.url}
-                               component={route.component}
-                               componentProps={route.props}
+                  component={route.component}
+                  componentProps={route.props}
                 />
               ))
             ))}
