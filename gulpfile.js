1e0920db1bec3d87340916b357e0f80653a7f800
Greg <gregory.pratt@me.com>
2017-11-08 16:03:29 +0000

    Simplified gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -7,27 +7,22 @@ runSequence = require('run-sequence'),
 source = ['src/scss/**/*.scss'],
 dest = 'src/assets/css'
 
-gulp.task('lint', () => gulp.src(source)
-  .pipe(sassLint({
-    configFile: './.scss-lint.yml'
-  }))
-  .pipe(sassLint.format())
-  .pipe(sassLint.failOnError())
-);
-
-gulp.task('build', () => {
-let build = gulp.src(source)
-  .pipe(sass())
-  .pipe(cssnano({
-    autoprefixer: {browsers: 'last 2 versions', add: true},
-    zindex: false
-  }))
-  .pipe(rename(function (path) {
-    path.extname = '.min.css'
-  }))
-  .pipe(gulp.dest(dest));
-
-return build;
-});
-
-gulp.task('default', done => runSequence('lint', 'build', done));
+gulp.task('default', () => gulp.src(source)
+// Lint
+.pipe(sassLint({
+  configFile: './.scss-lint.yml'
+}))
+.pipe(sassLint.format())
+.pipe(sassLint.failOnError())
+// Compile
+.pipe(sass())
+// Optimise
+.pipe(cssnano({
+  autoprefixer: {browsers: 'last 2 versions', add: true},
+  zindex: false
+}))
+// Distribute
+.pipe(rename(function (path) {
+  path.extname = '.min.css'
+}))
+.pipe(gulp.dest(dest)));
